873eecbe25c673d2a153a10866210e8b17fd02ac
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2024-07-18 13:03:18 +0900

    Refactor

--- Tests/SMBClientTests/SMBClientTests.swift
+++ Tests/SMBClientTests/SMBClientTests.swift
@@ -2,6 +2,8 @@ import XCTest
 @testable import SMBClient
 
 final class SMBClientTests: XCTestCase {
+  let fixtureURL = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+
   let alice = ServerConfig(username: "Alice", password: "alipass", share: "Alice Share", sharePath: "shares/alice")
   let bob = ServerConfig(username: "Bob", password: "bobpass", share: "Bob Share", sharePath: "shares/bob")
 
@@ -80,21 +82,20 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
-
     let files = try await client.listDirectory(path: "")
       .filter { $0.name != "." && $0.name != ".." }
       .sorted { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
 
     let fileManager = FileManager()
-    let testFiles = try fileManager.contentsOfDirectory(atPath: root.appending(component: user.sharePath).path(percentEncoded: false))
+    let root = fixtureURL.appending(component: user.sharePath)
+    let testFiles = try fileManager.contentsOfDirectory(atPath: root.path(percentEncoded: false))
       .sorted { $0.localizedStandardCompare($1) == .orderedAscending }
 
     for (actualFile, expectedFile) in zip(files, testFiles) {
       XCTAssertEqual(actualFile.name, expectedFile)
 
       var isDirectory: ObjCBool = false
-      fileManager.fileExists(atPath: root.appending(component: "\(user.sharePath)/\(expectedFile)").path(percentEncoded: false), isDirectory: &isDirectory)
+      fileManager.fileExists(atPath: root.appending(component: expectedFile).path(percentEncoded: false), isDirectory: &isDirectory)
       XCTAssertEqual(actualFile.isDirectory, isDirectory.boolValue)
     }
 
@@ -112,22 +113,21 @@ final class SMBClientTests: XCTestCase {
     let shareDirectory = user.sharePath
     try await client.treeConnect(path: share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
-
     func listDirectory(share: String, shareDirectory: String, path: String) async throws {
       let files = try await client.listDirectory(path: path)
         .filter { $0.name != "." && $0.name != ".." }
         .sorted { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
 
       let fileManager = FileManager()
-      let testFiles = try fileManager.contentsOfDirectory(atPath: root.appending(component: "\(shareDirectory)/\(path)").path(percentEncoded: false))
+      let root = fixtureURL.appending(component: "\(shareDirectory)/\(path)")
+      let testFiles = try fileManager.contentsOfDirectory(atPath: root.path(percentEncoded: false))
         .sorted { $0.localizedStandardCompare($1) == .orderedAscending }
 
       for (actualFile, expectedFile) in zip(files, testFiles) {
         XCTAssertEqual(actualFile.name, expectedFile)
 
         var isDirectory: ObjCBool = false
-        fileManager.fileExists(atPath: root.appending(component: "\(shareDirectory)/\(path)/\(expectedFile)").path(percentEncoded: false), isDirectory: &isDirectory)
+        fileManager.fileExists(atPath: root.appending(component: expectedFile).path(percentEncoded: false), isDirectory: &isDirectory)
         XCTAssertEqual(actualFile.isDirectory, isDirectory.boolValue)
 
         if actualFile.isDirectory {
@@ -155,22 +155,21 @@ final class SMBClientTests: XCTestCase {
     let shareDirectory = user.sharePath
     try await client.treeConnect(path: share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
-
     func listDirectory(share: String, shareDirectory: String, path: String) async throws {
       let files = try await client.listDirectory(path: path)
         .filter { $0.name != "." && $0.name != ".." }
         .sorted { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
 
       let fileManager = FileManager()
-      let testFiles = try fileManager.contentsOfDirectory(atPath: root.appending(component: "\(shareDirectory)/\(path)").path(percentEncoded: false))
+      let root = fixtureURL.appending(component: "\(shareDirectory)/\(path)")
+      let testFiles = try fileManager.contentsOfDirectory(atPath: root.path(percentEncoded: false))
         .sorted { $0.localizedStandardCompare($1) == .orderedAscending }
 
       for (actualFile, expectedFile) in zip(files, testFiles) {
         XCTAssertEqual(actualFile.name, expectedFile)
 
         var isDirectory: ObjCBool = false
-        fileManager.fileExists(atPath: root.appending(component: "\(shareDirectory)/\(path)/\(expectedFile)").path(percentEncoded: false), isDirectory: &isDirectory)
+        fileManager.fileExists(atPath: root.appending(component: expectedFile).path(percentEncoded: false), isDirectory: &isDirectory)
         XCTAssertEqual(isDirectory.boolValue, actualFile.isDirectory)
 
         if actualFile.isDirectory {
@@ -211,8 +210,8 @@ final class SMBClientTests: XCTestCase {
 
   func testListDirectory05() async throws {
     let client = SMBClient(host: "localhost", port: 4445)
-    try await client.login(username: "Alice", password: "alipass")
-    try await client.treeConnect(path: "Alice Share")
+    try await client.login(username: alice.username, password: alice.password)
+    try await client.treeConnect(path: alice.share)
 
     do {
       _ = try await client.listDirectory(path: "Nonexistent Directory")
@@ -279,10 +278,10 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let data = try await client.download(path: "test_files/file_example_JPG_1MB.jpg")
+    let path = "test_files/file_example_JPG_1MB.jpg"
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
-    XCTAssertEqual(data, try Data(contentsOf: root.appending(component: "\(user.sharePath)/test_files/file_example_JPG_1MB.jpg")))
+    let data = try await client.download(path: path)
+    XCTAssertEqual(data, try Data(contentsOf: fixtureURL.appending(component: "\(user.sharePath)/\(path)")))
   }
 
   func testRandomRead01() async throws {
@@ -291,10 +290,10 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+    let path = "test_files/file_example_TIFF_10MB.tiff"
 
-    let data = try Data(contentsOf: root.appending(component: "\(user.sharePath)/test_files/file_example_TIFF_10MB.tiff"))
-    let fileReader = client.fileReader(path: "test_files/file_example_TIFF_10MB.tiff")
+    let data = try Data(contentsOf: fixtureURL.appending(component: "\(user.sharePath)/\(path)"))
+    let fileReader = client.fileReader(path: path)
 
     do {
       let readData = try await fileReader.read(offset: 0, length: 1024)
@@ -320,10 +319,10 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+    let path = "test_files/file_example_TIFF_10MB.tiff"
 
-    let data = try Data(contentsOf: root.appending(component: "\(user.sharePath)/test_files/file_example_TIFF_10MB.tiff"))
-    let fileReader = client.fileReader(path: "test_files/file_example_TIFF_10MB.tiff")
+    let data = try Data(contentsOf: fixtureURL.appending(component: "\(user.sharePath)/\(path)"))
+    let fileReader = client.fileReader(path: path)
 
     do {
       let readData = try await fileReader.read(offset: 0)
@@ -341,10 +340,10 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+    let path = "test_files/file_example_MP4_1920_18MG.mp4"
 
-    let data = try Data(contentsOf: root.appending(component: "\(user.sharePath)/test_files/file_example_MP4_1920_18MG.mp4"))
-    let fileReader = client.fileReader(path: "test_files/file_example_MP4_1920_18MG.mp4")
+    let data = try Data(contentsOf: fixtureURL.appending(component: "\(user.sharePath)/\(path)"))
+    let fileReader = client.fileReader(path: path)
 
     let maxReadSize = client.session.maxReadSize
     let megaByte: UInt32 = 1024 * 1024
@@ -365,12 +364,12 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+    let path = "test_files/zip_10MB.zip"
 
     let directoryName: String = #function
     try await client.createDirectory(path: directoryName)
     try await client.upload(
-      localPath: root.appending(component: "shares/bob/test_files/zip_10MB.zip"),
+      localPath: fixtureURL.appending(component: "shares/bob/\(path)"),
       remotePath: "\(directoryName)/uploaded.zip"
     )
 
@@ -380,7 +379,7 @@ final class SMBClientTests: XCTestCase {
 
     XCTAssertEqual(
       downloadData,
-      try Data(contentsOf: root.appending(component: "shares/bob/test_files/zip_10MB.zip"))
+      try Data(contentsOf: fixtureURL.appending(component: "shares/bob/\(path)"))
     )
 
     try await client.deleteDirectory(path: directoryName)
@@ -393,11 +392,9 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
-
     let directoryName = #function
     try await client.upload(
-      localPath: root.appending(component: "shares/alice/test_data/dir1"),
+      localPath: fixtureURL.appending(component: "shares/alice/test_data/dir1"),
       remotePath: "test_files/\(directoryName)"
     )
 
@@ -415,12 +412,10 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
-
     let directoryName = #function
     try await client.createDirectory(path: "test_files/\(directoryName)")
 
-    let localPath = root.appending(component: "shares/alice/test_data/dir1")
+    let localPath = fixtureURL.appending(component: "shares/alice/test_data/dir1")
     let remotePath = "test_files/\(directoryName)"
     try await client.upload(
       localPath: localPath,
@@ -445,12 +440,12 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+    let path = "test_files/file_example_PPT_1MB.ppt"
 
     let directoryName: String = #function
     try await client.createDirectory(path: directoryName)
     try await client.upload(
-      localPath: root.appending(component: "shares/bob/test_files/file_example_PPT_1MB.ppt"),
+      localPath: fixtureURL.appending(component: "shares/bob/\(path)"),
       remotePath: "\(directoryName)/uploaded.ppt"
     )
 
@@ -459,7 +454,7 @@ final class SMBClientTests: XCTestCase {
     let downloadData = try await client.download(path: "\(directoryName)/uploaded.ppt")
     XCTAssertEqual(
       downloadData,
-      try Data(contentsOf: root.appending(component: "shares/bob/test_files/file_example_PPT_1MB.ppt"))
+      try Data(contentsOf: fixtureURL.appending(component: "shares/bob/\(path)"))
     )
 
     try await client.deleteFile(path: "\(directoryName)/uploaded.ppt")
@@ -476,32 +471,31 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+    let path = "test_files/file_example_ODS_5000.ods"
+    let expedtedData = try Data(contentsOf: fixtureURL.appending(component: "shares/bob/\(path)"))
 
     let directoryName: String = #function
     try await client.createDirectory(path: directoryName)
     try await client.upload(
-      localPath: root.appending(component: "shares/bob/test_files/file_example_ODS_5000.ods"),
+      localPath: fixtureURL.appending(component: "shares/bob/\(path)"),
       remotePath: "\(directoryName)/uploaded.ods"
     )
 
     try await assertFileExists(at: "\(directoryName)/uploaded.ods", client: client)
 
-    let downloadData = try await client.download(path: "\(directoryName)/uploaded.ods")
-    XCTAssertEqual(
-      downloadData,
-      try Data(contentsOf: root.appending(component: "shares/bob/test_files/file_example_ODS_5000.ods"))
-    )
+    do {
+      let downloadData = try await client.download(path: "\(directoryName)/uploaded.ods")
+      XCTAssertEqual(downloadData, expedtedData)
+    }
 
     try await client.rename(from: "\(directoryName)/uploaded.ods", to: "\(directoryName)/renamed.ods")
     try await assertFileDoesNotExist(at: "\(directoryName)/uploaded.ods", client: client)
     try await assertFileExists(at: "\(directoryName)/renamed.ods", client: client)
 
-    let downloadData2 = try await client.download(path: "\(directoryName)/renamed.ods")
-    XCTAssertEqual(
-      downloadData2,
-      try Data(contentsOf: root.appending(component: "shares/bob/test_files/file_example_ODS_5000.ods"))
-    )
+    do {
+      let downloadData = try await client.download(path: "\(directoryName)/renamed.ods")
+      XCTAssertEqual(downloadData, expedtedData)
+    }
 
     try await client.deleteDirectory(path: directoryName)
     try await assertDirectoryDoesNotExist(at: directoryName, client: client)
@@ -514,22 +508,22 @@ final class SMBClientTests: XCTestCase {
     try await client.login(username: user.username, password: user.password)
     try await client.treeConnect(path: user.share)
 
-    let root = Bundle.module.url(forResource: "Fixtures", withExtension: nil)!
+    let path = "test_files/file_example_PNG_2500kB.jpg"
+    let expedtedData = try Data(contentsOf: fixtureURL.appending(component: "shares/bob/\(path)"))
 
     let directoryName: String = #function
     try await client.createDirectory(path: directoryName)
     try await client.upload(
-      localPath: root.appending(component: "shares/bob/test_files/file_example_PNG_2500kB.jpg"),
+      localPath: fixtureURL.appending(component: "shares/bob/\(path)"),
       remotePath: "\(directoryName)/uploaded.jpg"
     )
 
     try await assertFileExists(at: "\(directoryName)/uploaded.jpg", client: client)
 
-    let downloadData = try await client.download(path: "\(directoryName)/uploaded.jpg")
-    XCTAssertEqual(
-      downloadData,
-      try Data(contentsOf: root.appending(component: "shares/bob/test_files/file_example_PNG_2500kB.jpg"))
-    )
+    do {
+      let downloadData = try await client.download(path: "\(directoryName)/uploaded.jpg")
+      XCTAssertEqual(downloadData, expedtedData)
+    }
 
     try await client.createDirectory(path: "\(directoryName)/subdir")
     try await client.move(from: "\(directoryName)/uploaded.jpg", to: "\(directoryName)/subdir/moved.jpg")
@@ -537,11 +531,10 @@ final class SMBClientTests: XCTestCase {
     try await assertFileDoesNotExist(at: "\(directoryName)/uploaded.jpg", client: client)
     try await assertFileExists(at: "\(directoryName)/subdir/moved.jpg", client: client)
 
-    let downloadData2 = try await client.download(path: "\(directoryName)/subdir/moved.jpg")
-    XCTAssertEqual(
-      downloadData2,
-      try Data(contentsOf: root.appending(component: "shares/bob/test_files/file_example_PNG_2500kB.jpg"))
-    )
+    do {
+      let downloadData = try await client.download(path: "\(directoryName)/subdir/moved.jpg")
+      XCTAssertEqual(downloadData, expedtedData)
+    }
 
     try await client.deleteDirectory(path: directoryName)
     try await assertDirectoryDoesNotExist(at: directoryName, client: client)
