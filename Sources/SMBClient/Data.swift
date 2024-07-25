3fa38b2a4344afa822f56a993793826ca136d88a
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2024-07-25 11:29:34 +0900

    Refactor

--- Sources/SMBClient/Data.swift
+++ Sources/SMBClient/Data.swift
@@ -25,3 +25,25 @@ extension Int16: BinaryConvertible {}
 extension Int32: BinaryConvertible {}
 extension Int64: BinaryConvertible {}
 extension Int: BinaryConvertible {}
+
+extension Data {
+  init?(_ hex: String) {
+    let len = hex.count / 2
+    var data = Data(capacity: len)
+    for i in 0..<len {
+      let j = hex.index(hex.startIndex, offsetBy: i * 2)
+      let k = hex.index(j, offsetBy: 2)
+      let bytes = hex[j..<k]
+      if var num = UInt8(bytes, radix: 16) {
+        data.append(&num, count: 1)
+      } else {
+        return nil
+      }
+    }
+    self = data
+  }
+
+  var hex: String {
+    return reduce("") { $0 + String(format: "%02x", $1) }
+  }
+}
