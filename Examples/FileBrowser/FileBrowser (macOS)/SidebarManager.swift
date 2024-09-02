74bd7d551002f8d8710c2d87acccaa74bf9deccd
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2024-09-02 16:40:28 +0900

    Improve Windows file sharing compatibility

--- Examples/FileBrowser/FileBrowser (macOS)/SidebarManager.swift
+++ Examples/FileBrowser/FileBrowser (macOS)/SidebarManager.swift
@@ -135,7 +135,7 @@ extension SidebarManager {
         
         do {
           let shares = try await session.client.listShares()
-            .filter { $0.type.contains(.diskTree) && !$0.type.contains(.ipc) }
+            .filter { $0.type.contains(.diskTree) && !$0.type.contains(.special) }
             .sorted { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
             .map { ShareNode(id: ID("\(serverRoot)/\($0.name)"), device: serverNode.name, name: $0.name, parent: serverNode.id) }
 
